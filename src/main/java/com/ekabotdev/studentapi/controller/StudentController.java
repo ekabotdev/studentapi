7d43f0501e6970f9e28de50266fd0b4e0cd44811
ekabotdev <ekabotdev@gmail.com>
2026-06-24 14:27:42 +0300

    Added StudentController

---
+++ src/main/java/com/ekabotdev/studentapi/controller/StudentController.java
@@ -0,0 +1,26 @@
+package com.ekabotdev.studentapi.controller;
+
+import com.ekabotdev.studentapi.entity.Student;
+import com.ekabotdev.studentapi.service.StudentService;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/students")
+public class StudentController {
+
+    private final StudentService studentService;
+
+    public StudentController(StudentService studentService) {
+        this.studentService = studentService;
+    }
+    @PostMapping
+    public Student createStudent(@RequestBody Student student) {
+        return studentService.creatStudent(student);
+    }
+    @GetMapping
+    public List<Student> findAllStudents() {
+        return studentService.getAllStudents();
+    }
+}
