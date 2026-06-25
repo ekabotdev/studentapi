0096ff5d46d99db5ea66c322427c84cb280dff8d
ekabotdev <ekabotdev@gmail.com>
2026-06-25 09:16:37 +0300

    Refactor StudentResponse

--- src/main/java/com/ekabotdev/studentapi/dto/StudentResponse.java
+++ src/main/java/com/ekabotdev/studentapi/dto/StudentResponse.java
@@ -2,18 +2,16 @@ package com.ekabotdev.studentapi.dto;
 
 import lombok.Getter;
 import lombok.Setter;
+@Getter
+@Setter
 
 public class StudentResponse {
-    @Getter
-    @Setter
+
     private Long id;
-    @Getter
-    @Setter
+
     private String firstName;
-    @Getter
-    @Setter
+
     private String lastName;
-    @Getter
-    @Setter
+
     private String email;
 }
